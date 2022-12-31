5e329f07243750b92f3e5a2f7cf448fc01d80527
Stanley John <stanleyjohn98@gmail.com>
2023-01-01 00:29:06 +0530

    planning of db design models - in progress

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class GroupTransaction(models.Model):
+    payer = models.ForeignKey(User, default = 'Deleted user', on_delete = models.SET_DEFAULT)
+    item = models.CharField(max_length = 100)
+    category = models.CharField(max_length = 50)
+    amount = models.IntegerField()
+    content = models.TextField()
+    transaction_date = models.DateTimeField(add_now = True)
+
+
+
+class Transaction(models.Model):
+    payer = models.ForeignKey(User, default = 'Deleted user', on_delete = models.SET_DEFAULT)
+    item = models.CharField(max_length = 100)
+    category = models.CharField(max_length = 50)
+    amount = models.IntegerField()
+    content = models.TextField()
+    transaction_date = models.DateTimeField(add_now = True)
